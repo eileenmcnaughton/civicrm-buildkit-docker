df5d94a0fc4b89edb282667a92cb18f3c97e26f3
eileen <emcnaughton@wikimedia.org>
2020-11-18 11:00:47 +1300

    Add dmaster drupal aliase

--- civicrm/drush/site-aliases/aliases.drushrc.php
+++ civicrm/drush/site-aliases/aliases.drushrc.php
@@ -3,3 +3,7 @@ $aliases['wmff'] = [
     'root' => '/buildkit/build/wmff/drupal',
     'uri' => 'http://wmff.localhost:7979/',
 ];
+$aliases['dmaster'] = [
+    'root' => '/buildkit/build/dmaster/web',
+    'uri' => 'http://wmff.dmaster:7979/',
+];

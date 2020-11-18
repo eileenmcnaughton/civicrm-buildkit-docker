cfbaacd1e8fd8dd3556dbf46e7c1087772666dda
eileen <emcnaughton@wikimedia.org>
2020-11-18 22:28:38 +1300

    Fix aliases

--- publish/civicrm/php7.3/drush/site-aliases/aliases.drushrc.php
+++ publish/civicrm/php7.3/drush/site-aliases/aliases.drushrc.php
@@ -1,5 +1,9 @@
 <?php
 $aliases['wmff'] = [
-    'root' => '/buildkit/build/wmff/drupal',
-    'uri' => 'http://wmff.localhost:7979/',
+  'root' => '/buildkit/build/wmff/drupal',
+  'uri' => 'http://wmff.localhost:7979/',
+];
+$aliases['dmaster'] = [
+  'root' => '/buildkit/build/dmaster/web',
+  'uri' => 'http://wmff.dmaster:7979/',
 ];
